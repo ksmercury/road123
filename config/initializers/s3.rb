cd6490f89fe3bc41e7146f65433414fd6bb96fb4
Kim Sarkies <meaculpa123ks@hotmail.com>
2011-04-08 17:46:26 -0400

    ten commit

--- config/initializers/s3.rb
+++ config/initializers/s3.rb
@@ -1,6 +1,5 @@
 AWS::S3::Base.establish_connection!(
  :access_key_id     => ENV['AKIAIVYHAEHKF5LHVGVQ'],
-  :secret_access_key => ENV['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'],
-   :bucket => ENV['edw']
+  :secret_access_key => ENV['MufBzBBm9lTvhO588tBk6TlvjVkgMEJCpGoRsM6D']
 )
 
