c4eb4a2fa8d2dfccca38b7353985bb779efdcaf0
Kim Sarkies <meaculpa123ks@hotmail.com>
2011-04-08 17:41:10 -0400

    nine commit

--- config/initializers/s3.rb
+++ config/initializers/s3.rb
@@ -1,9 +1,6 @@
-#AWS::S3::Base.establish_connection!(
- # :access_key_id     => ENV['AKIAIVYHAEHKF5LHVGVQ'],
- # :secret_access_key => ENV['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']
-#)
+AWS::S3::Base.establish_connection!(
+ :access_key_id     => ENV['AKIAIVYHAEHKF5LHVGVQ'],
+  :secret_access_key => ENV['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'],
+   :bucket => ENV['edw']
+)
 
-
-Rails.env == "production" S3_CREDENTIALS = {
- :access_key_id => ENV['AKIAIVYHAEHKF5LHVGVQ'], :secret_access_key => ENV['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'], :bucket => "edw"
- } 
