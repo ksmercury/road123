066424f1610570b771de0fce8c8a2392b089796f
Kim Sarkies <meaculpa123ks@hotmail.com>
2011-04-08 15:36:12 -0400

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Road123::Application.config.secret_token = 'c6653e6086efbc608090d49bf2a903695a6f15ff84a983d9f5f0e5719ec249f236102edc5be2593970ebb805236593e80598707d29e7db6cf7254fd08247654b'
