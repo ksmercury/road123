ce0ed95dfe406444d95918054ef89dec7f6d3cbe
Kim Sarkies <meaculpa123ks@hotmail.com>
2011-04-09 13:03:47 -0400

    thirdthird commit

--- config/environment.rb
+++ config/environment.rb
@@ -1,5 +1,5 @@
 # Load the rails application
 require File.expand_path('../application', __FILE__)
-
+#config.gem "rmagick", :lib => "RMagick"
 # Initialize the rails application
 Road123::Application.initialize!
