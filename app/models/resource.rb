065e9ca54af068ca6fc19be2bd997ec1fe2d2571
Kim Sarkies <meaculpa123ks@hotmail.com>
2011-04-09 10:36:49 -0400

    oneone commit

--- app/models/resource.rb
+++ app/models/resource.rb
@@ -1,6 +1,8 @@
 class Resource < ActiveRecord::Base
   belongs_to :attachable
   
-  has_attached_file :photo, :storage => :s3, :s3_credentials => S3_CREDENTIALS
+  has_attached_file :attachment, 
+  :storage => :s3,
+  :s3_credentials => S3_CREDENTIALS
   
 end
